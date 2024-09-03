e39b04ccaaa6dc44030018fc83266b30b9dc9380
Neea <neearusch@gmail.com>
2024-09-02 21:27:06 -0400

    try escape

--- .github/utils/pr.sh
+++ .github/utils/pr.sh
@@ -7,13 +7,15 @@ abs=$(bash .github/utils/abs.sh)
 # construct a pull request body text
 body="This paper was randomly selected as your next reading.
 
-### ${tle}
+\#\#\# ${tle}
 
 ${abs}
 
 ${mla}
 
 **Merge this PR to apply selection.**"
+
+body=$(echo "$body" | sed 's/[^a-z“\.,;:\/ \#0-9A-Z]//g')
 
 # output to terminal
 echo "$body"
