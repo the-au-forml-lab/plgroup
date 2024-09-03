4f58b480bd2c085ef215410ebad3d24e4b59b6d7
Neea <neearusch@gmail.com>
2024-09-02 21:29:18 -0400

    -\n

--- .github/utils/pr.sh
+++ .github/utils/pr.sh
@@ -5,16 +5,7 @@ mla=$(bash .github/utils/mla.sh)
 abs=$(bash .github/utils/abs.sh)
 
 # construct a pull request body text
-body="This paper was randomly selected as your next reading.
-
-\#\#\# ${tle}
-
-${abs}
-
-${mla}
-
-**Merge this PR to apply selection.**"
-
+body="This paper was randomly selected as your next reading.\n\n###${tle}\n\n${abs}\n\n${mla}\n\n**Merge this PR to apply selection.**"
 body=$(echo "$body" | sed 's/[^a-z“\.,;:\/ \#0-9A-Z]//g')
 
 # output to terminal
