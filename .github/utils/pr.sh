8b4607e5bbd4c18e877184d0217fdc0bfdde0479
Neea <neearusch@gmail.com>
2024-09-02 21:49:03 -0400

    formatting fix

--- .github/utils/pr.sh
+++ .github/utils/pr.sh
@@ -5,8 +5,8 @@ mla=$(bash .github/utils/mla.sh)
 abs=$(bash .github/utils/abs.sh)
 
 # construct a pull request body text
-body="This paper was randomly selected as your next reading.\n\n###${tle}\n\n${abs}\n\n${mla}\n\n**Merge this PR to apply selection.**"
-body=$(echo "$body" | sed 's/[^a-z“\.,;:\/ \#0-9A-Z]//g')
+body="This paper was randomly selected as your next reading.\n\n### ${tle}\n\n${abs}\n\n${mla}\n\n**Merge this PR to apply selection.**"
+body=$(echo "$body" | sed 's/[^a-z“\.,;:\/ \*\#0-9A-Z]//g')
 
 # output to terminal
 echo "$body"
