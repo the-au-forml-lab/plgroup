abee34f357e371f58b90ba857a0eca14e9274253
Peter Hanukaev <phanukaev@gmail.com>
2025-06-21 21:22:04 -0400

    Get rid of workflow utility scripts
    we can do better from inside node

--- .github/utils/title.sh
+++
@@ -1,3 +0,0 @@
-# change to git root if not there
-cd "$(git rev-parse --show-toplevel)" || return
-sed -n '1p' ./data/desc.txt
--- .github/utils/voting.sh
+++
@@ -1,3 +0,0 @@
-# change to git root if not there
-cd "$(git rev-parse --show-toplevel)" || return
-awk '{printf "%s\\n", $0}' .github/assets/voting.md
