618d103f86861a2cd6985c718fae70695f168784
Peter Hanukaev <phanukaev@gmail.com>
2025-06-26 14:03:22 -0400

    minor tweak to max_redirects config option

--- src/request.ts
+++ src/request.ts
@@ -1,7 +1,7 @@
 import https from 'https';
 import {OutgoingHttpHeaders} from 'http';
 import {log, LogLv as LogLv} from './util.js';
-import {MAX_REDIRECTS} from './config.js';
+import {CONFIG} from './config.js';
 
 export type Headers = OutgoingHttpHeaders;
 
@@ -15,7 +15,7 @@ export function readURL(url: string|URL, headers: Headers={}): Promise<string>
         const req = (v: URL, redirects: number = 0) => {
             https.get(v, {headers}, res => {
                 if(res.statusCode === 302){
-                    if(redirects < MAX_REDIRECTS){
+                    if(redirects < CONFIG.MAX_REDIRECTS){
                         const newUrl = URL.parse(res.headers.location!, url);
                         log(LogLv.debug, `... redirecting to ${newUrl}`);
                         req(newUrl!, redirects+1);
